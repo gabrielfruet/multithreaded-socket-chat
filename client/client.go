8075fd75d63753b869988a622c4ff84d57874a19
gabrielfruet <gabrielfruet538@gmail.com>
2024-08-01 14:36:39 -0300

    quase tudo

--- client/client.go
+++ client/client.go
@@ -1,24 +1,67 @@
 package main
 
 import (
+    "os"
+    "bufio"
     "fmt"
     "net"
 )
 
+func msgReceiver(conn net.Conn) {
+    for {
+        buf := make([]byte, 1024)
+
+        _, err := conn.Read(buf)
+        if err != nil {
+            fmt.Println(err)
+            return
+        }
+
+        fmt.Printf("%s\n", buf)
+    } 
+}
+
+func msgSender(conn net.Conn) {
+    scanner := bufio.NewScanner(os.Stdin)
+    for scanner.Scan() {
+        text := scanner.Text()
+
+        if text == "/disconnect" {
+            fmt.Println("Disconnecting.")
+            return
+        }
+
+        _, err := conn.Write([]byte(text))
+
+        if err != nil {
+            fmt.Println(err)
+            return
+        }
+    }
+}
+
 func main() {
     conn, err := net.Dial("tcp", "localhost:8080")
+    defer conn.Close()
 
     if err != nil {
         fmt.Println(err)
         return
     }
 
-    _, err = conn.Write([]byte("Hello server!"))
+    scanner := bufio.NewScanner(os.Stdin)
+
+    fmt.Println("Enter your username")
+
+    scanner.Scan()
+    username := scanner.Text()
+    _, err = conn.Write([]byte(username))
 
     if err != nil {
         fmt.Println(err)
         return
     }
 
-    conn.Close()
+    go msgSender(conn)
+    msgReceiver(conn)
 }
--- server/server.go
+++ server/server.go
@@ -3,7 +3,43 @@ package main
 import (
     "fmt"
     "net"
+    "sync"
 )
+
+type Chat struct {
+    clients map[string]net.Conn
+    sends []chan string
+    sends_mutex sync.Mutex
+}
+
+func (c *Chat) AddClient(username string, conn net.Conn, send chan string) {
+    c.sends_mutex.Lock()
+    defer c.sends_mutex.Unlock()
+    c.clients[username] = conn
+    //c.sends[username] = send
+    c.sends = append(c.sends, send)
+}
+
+func (c Chat) SendToClients(msg string) {
+    c.sends_mutex.Lock()
+    defer c.sends_mutex.Unlock()
+    for _, sendChan := range c.sends {
+        sendChan <- msg
+    }
+}
+
+func createChat() Chat {
+    clients := make(map[string]net.Conn)
+    sends := make([]chan string, 0)
+    return Chat { clients, sends, sync.Mutex{} }
+}
+
+
+type Client struct {
+    chat *Chat
+    conn net.Conn
+}
+
 
 func main() {
     ln, err := net.Listen("tcp",":8080")
@@ -13,7 +49,10 @@ func main() {
         return
     }
 
+    mainchat := createChat()
+
     for {
+        fmt.Println(mainchat.sends)
         conn, err := ln.Accept()
 
         if err != nil {
@@ -21,21 +60,62 @@ func main() {
             continue
         }
 
-        go handleConnection(conn)
+        client := Client {&mainchat, conn}
+
+        go client.HandleConnection()
     }
 }
 
-func handleConnection(conn net.Conn) {
+func (c Client) SendMsg(recv chan string) {
+    for {
+        msg := <-recv
+        _, err := c.conn.Write([]byte(msg))
+
+        if err != nil {
+            fmt.Println(err)
+            return
+        }
+    } 
+}
+
+func (c Client) ReceiveMsg() {
+
+    for {
+
+        buf := make([]byte, 1024)
+
+        _, err := c.conn.Read(buf)
+        if err != nil {
+            fmt.Println(err)
+            return
+        }
+
+        fmt.Printf("Received: %s\n", buf)
+        go c.chat.SendToClients(string(buf))
+    }
+    
+}
+
+func (c Client) HandleConnection() {
+    conn := c.conn
+
     defer conn.Close()
 
-    buf := make([]byte, 1024)
+    send := make(chan string)
 
-    _, err := conn.Read(buf)
+    username := make([]byte, 128)
+
+    _, err := conn.Read(username)
+
     if err != nil {
         fmt.Println(err)
         return
     }
 
-    fmt.Printf("Received: %s", buf)
+    c.chat.AddClient(string(username), conn, send)
+    fmt.Printf("%s logged in\n", username)
+
+    go c.ReceiveMsg()
+    c.SendMsg(send)
 }
 

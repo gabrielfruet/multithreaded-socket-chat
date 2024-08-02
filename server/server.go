b26b6b3d2d8f9e1db9764feda89c96fa0ca93dfa
gabrielfruet <gabrielfruet538@gmail.com>
2024-08-02 13:59:40 -0300

    refactor: changed how objects are passed to chat

--- server/server.go
+++ server/server.go
@@ -6,38 +6,51 @@ import (
     "sync"
 )
 
+type Message struct {
+    username, text string
+}
+
+func (m Message) String() string {
+    return fmt.Sprintf("%s: %s", m.username, m.text)
+}
+
 type Chat struct {
-    clients map[string]net.Conn
-    sends []chan string
+    clients map[string]Client
+    clients_connections map[string]net.Conn
+    sends map[string]chan Message
     sends_mutex sync.Mutex
 }
 
-func (c *Chat) AddClient(username string, conn net.Conn, send chan string) {
+func (c *Chat) AddClient(client Client) {
     c.sends_mutex.Lock()
     defer c.sends_mutex.Unlock()
-    c.clients[username] = conn
+    //c.clients_connections[username] = conn
     //c.sends[username] = send
-    c.sends = append(c.sends, send)
+    c.clients[client.username] = client
+    //c.sends = append(c.sends, send)
 }
 
-func (c Chat) SendToClients(msg string) {
+func (c *Chat) SendToClients(msg Message) {
     c.sends_mutex.Lock()
     defer c.sends_mutex.Unlock()
-    for _, sendChan := range c.sends {
-        sendChan <- msg
+    for _, client := range c.clients {
+        client.send <- msg
     }
 }
 
 func createChat() Chat {
-    clients := make(map[string]net.Conn)
-    sends := make([]chan string, 0)
-    return Chat { clients, sends, sync.Mutex{} }
+    clients_connections := make(map[string]net.Conn)
+    clients := make(map[string]Client)
+    sends := make(map[string]chan Message, 0)
+    return Chat { clients, clients_connections, sends, sync.Mutex{} }
 }
 
 
 type Client struct {
     chat *Chat
     conn net.Conn
+    username string
+    send chan Message
 }
 
 
@@ -52,7 +65,6 @@ func main() {
     mainchat := createChat()
 
     for {
-        fmt.Println(mainchat.sends)
         conn, err := ln.Accept()
 
         if err != nil {
@@ -60,16 +72,33 @@ func main() {
             continue
         }
 
-        client := Client {&mainchat, conn}
 
-        go client.HandleConnection()
+        username := make([]byte, 128)
+
+        _, err = conn.Read(username)
+
+        if err != nil {
+            fmt.Println(err)
+            return
+        }
+
+        send := make(chan Message)
+
+        client := Client {&mainchat, conn, string(username), send}
+
+        client.chat.AddClient(client)
+
+        fmt.Printf("%s logged in\n", username)
+
+        go client.ReceiveMsg()
+        go client.SendMsg(send)
     }
 }
 
-func (c Client) SendMsg(recv chan string) {
+func (c Client) SendMsg(recv chan Message) {
     for {
         msg := <-recv
-        _, err := c.conn.Write([]byte(msg))
+        _, err := c.conn.Write([]byte(msg.String()))
 
         if err != nil {
             fmt.Println(err)
@@ -90,32 +119,9 @@ func (c Client) ReceiveMsg() {
             return
         }
 
+        msg := Message{c.username, string(buf)}
+
         fmt.Printf("Received: %s\n", buf)
-        go c.chat.SendToClients(string(buf))
+        go c.chat.SendToClients(msg)
     }
-    
 }
-
-func (c Client) HandleConnection() {
-    conn := c.conn
-
-    defer conn.Close()
-
-    send := make(chan string)
-
-    username := make([]byte, 128)
-
-    _, err := conn.Read(username)
-
-    if err != nil {
-        fmt.Println(err)
-        return
-    }
-
-    c.chat.AddClient(string(username), conn, send)
-    fmt.Printf("%s logged in\n", username)
-
-    go c.ReceiveMsg()
-    c.SendMsg(send)
-}
-

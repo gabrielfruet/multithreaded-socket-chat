0fcd8eead535508fb35dc4dadd02df9cc37d6e08
gabrielfruet <gabrielfruet538@gmail.com>
2024-08-02 16:40:09 -0300

    refactor: refactored functions into files

---
+++ server/chat.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"sync"
+	"errors"
+)
+
+type Chat struct {
+    clients map[string]Client
+    clients_mutex sync.Mutex
+}
+
+func (c *Chat) RemoveClient(username string) {
+    c.clients_mutex.Lock()
+    delete(c.clients, username)
+    c.clients_mutex.Unlock()
+}
+
+func (c *Chat) AddClient(client Client) error {
+    c.clients_mutex.Lock()
+    defer c.clients_mutex.Unlock()
+
+    _, exists := c.clients[client.username]
+
+    if exists {
+        return errors.New("Client already existed.")
+    }
+
+    c.clients[client.username] = client
+
+    return nil
+}
+
+func (c *Chat) SendToClients(msg Message) {
+    c.clients_mutex.Lock()
+    defer c.clients_mutex.Unlock()
+    for username, client := range c.clients {
+        if msg.username != username {
+            client.send <- msg
+        }
+    }
+}
+
+func createChat() Chat {
+    clients := make(map[string]Client)
+    return Chat { clients, sync.Mutex{} }
+}
--- server/server.go
+++ server/server.go
@@ -12,64 +12,6 @@ const (
     CONNECTION_UNSUCCESFULL="ERR"
 )
 
-type Message struct {
-    username, text string
-}
-
-func (m Message) String() string {
-    return fmt.Sprintf("%s: %s", m.username, m.text)
-}
-
-type Chat struct {
-    clients map[string]Client
-    clients_mutex sync.Mutex
-}
-
-func (c *Chat) RemoveClient(username string) {
-    c.clients_mutex.Lock()
-    delete(c.clients, username)
-    c.clients_mutex.Unlock()
-}
-
-func (c *Chat) AddClient(client Client) error {
-    c.clients_mutex.Lock()
-    defer c.clients_mutex.Unlock()
-
-    _, exists := c.clients[client.username]
-
-    if exists {
-        return errors.New("Client already existed.")
-    }
-
-    c.clients[client.username] = client
-
-    return nil
-}
-
-func (c *Chat) SendToClients(msg Message) {
-    c.clients_mutex.Lock()
-    defer c.clients_mutex.Unlock()
-    for username, client := range c.clients {
-        if msg.username != username {
-            client.send <- msg
-        }
-    }
-}
-
-func createChat() Chat {
-    clients := make(map[string]Client)
-    return Chat { clients, sync.Mutex{} }
-}
-
-
-type Client struct {
-    chat *Chat
-    conn net.Conn
-    username string
-    send chan Message
-}
-
-
 func main() {
     ln, err := net.Listen("tcp",":8080")
 
@@ -126,49 +68,5 @@ func main() {
                 break
             }
         }
-
-
-
-    }
-}
-
-func (c Client) SendMsgToClient() {
-    for {
-        msg := <-c.send
-        _, err := c.conn.Write([]byte(msg.String()))
-
-        if err != nil {
-            fmt.Println(err)
-            c.Disconnect()
-            return
-        }
-    } 
-}
-
-func (c Client) Disconnect() {
-    c.conn.Close()
-    c.chat.SendToClients(Message{ c.username, "Disconnected..."})
-    fmt.Printf("%s disconnected.", c.username)
-    c.chat.RemoveClient(c.username)
-}
-
-func (c Client) ReceiveMsgFromClient() {
-
-    for {
-
-        buf := make([]byte, 1024)
-
-        _, err := c.conn.Read(buf)
-
-        if err != nil {
-            fmt.Println(err)
-            c.Disconnect()
-            return
-        }
-
-        msg := Message{c.username, string(buf)}
-
-        fmt.Printf("Received: %s\n", buf)
-        go c.chat.SendToClients(msg)
     }
 }

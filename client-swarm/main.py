5927faaec7bb6c6b31119bf39be0e0d4daf67a74
gabrielfruet <gabrielfruet538@gmail.com>
2024-08-09 08:57:57 -0300

    feat: added client swarn and fixed disconnect bug

---
+++ client-swarm/main.py
@@ -0,0 +1,68 @@
+import socket
+import time
+import random
+import threading
+
+NO_OF_THREADS=256
+
+def client(names, phrases):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_addr = ('localhost', 8080)
+    sock.connect(server_addr)
+
+    while True:
+        name = random.choice(names)
+        room = random.randint(0,100)
+        try:
+            sock.send(str(room).encode('utf-8'))
+            status = sock.recv(4)
+            if status == 'ERR':
+                continue
+            sock.send(str(name).encode('utf-8'))
+            status = sock.recv(4)
+            if status == 'ERR':
+                continue
+
+            break
+
+        except Exception as e:
+            print(e)
+            sock.close()
+            return
+
+
+    while True:
+        try:
+            phrase = random.choice(phrases)
+            sock.send(str(phrase).encode('utf-8'))
+            time.sleep(random.randint(4,10))
+        except Exception as e:
+            print(e)
+            sock.close()
+            return
+
+def main():
+    names = []
+    with open('names.txt', "r", encoding='utf-8') as namestxt:
+        while name := namestxt.readline():
+            names.append(name.strip())
+
+    phrases = []
+    with open('phrases.txt', 'r', encoding='utf-8') as phrasestxt:
+        while phrase := phrasestxt.readline():
+            phrases.append(phrase)
+
+    threads = []
+
+    for i in range(NO_OF_THREADS):
+        thread = threading.Thread(target=client, args=(names,phrases))
+        thread.start()
+        time.sleep(0.1)
+        threads.append(thread)
+
+    for thread in threads:
+        thread.join()
+
+
+if __name__ == '__main__':
+    main()
